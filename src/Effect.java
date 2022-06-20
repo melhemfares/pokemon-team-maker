5679781c3995c19e283654f4dd3fa4f080015cfa
Melhem Fares <faresm0305@gmail.com>
2022-06-20 15:36:36 -0400

    Initial commit

---
+++ src/Effect.java
@@ -0,0 +1,11 @@
+public enum Effect {
+    NONE,
+    PARALYZE,
+    POISON,
+    BURN,
+    FREEZE,
+    FLINCH,
+    CONFUSE,
+    INFATUATE,
+    LEECH
+}
---
+++ src/Type.java
@@ -0,0 +1,20 @@
+public enum Type {
+    NORMAL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DARK,
+    DRAGON,
+    STEEL,
+    FAIRY
+}
